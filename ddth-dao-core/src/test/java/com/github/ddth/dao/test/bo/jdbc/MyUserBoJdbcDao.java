f0779efb3583ceadeb62485138b6c113cd153786
Thanh Nguyen <btnguyen2k@gmail.com>
2017-08-11 18:06:28 +0700

    Minor fix

---
+++ ddth-dao-core/src/test/java/com/github/ddth/dao/test/bo/jdbc/MyUserBoJdbcDao.java
@@ -0,0 +1,8 @@
+package com.github.ddth.dao.test.bo.jdbc;
+
+public class MyUserBoJdbcDao extends UserBoJdbcDao {
+    public static void main(String[] args) {
+        MyUserBoJdbcDao dao = new MyUserBoJdbcDao();
+        System.out.println(dao);
+    }
+}
